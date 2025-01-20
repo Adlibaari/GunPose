0032f2d80fb795550b28ba90ba08c1b5c006100e
Adlibaari <93728649+Adlibaari@users.noreply.github.com>
2025-01-20 09:00:05 +0700

    Update step2b.py

--- step2b.py
+++ step2b.py
@@ -1,46 +1,38 @@
-
-import os
-
-# Path ke folder dataset
-dataset_path = 'C:/Users/Barry/Documents/Uni/Projects/Object Tracking/Pose Estimation/Gunestimation/XGboost/level2/dataset/'
-
-# Path ke folder cutting
-cutting_path = os.path.join(dataset_path, 'gun')
-# List semua file di dalam folder cutting
-cutting_files = os.listdir(cutting_path)
-
-# Path ke folder non_cutting
-non_cutting_path = os.path.join(dataset_path, 'nongun')
-# List semua file di dalam folder non_cutting
-non_cutting_files = os.listdir(non_cutting_path)
-
-# Menampilkan list nama file di folder cutting
-print("Files in cutting folder:", cutting_files)
-
-# Menampilkan list nama file di folder non_cutting
-print("Files in non_cutting folder:", non_cutting_files)
-
 import pandas as pd
 import os
 
-df = pd.read_csv('C:/Users/Barry/Documents/Uni/Projects/Object Tracking/Pose Estimation/Gunestimation/XGboost/level2/keypoints.csv')
+df = pd.read_csv('/path/to/your/directory/level2/keypoints.csv')
 
-# Path ke folder dataset
-dataset_path = 'C:/Users/Barry/Documents/Uni/Projects/Object Tracking/Pose Estimation/Gunestimation/XGboost/level2/dataset/'
-cutting_path = os.path.join(dataset_path, 'gun')
-non_cutting_path = os.path.join(dataset_path, 'nongun')
+# Path to dataset folder 
+dataset_path = '/path/to/your/directory/level2/dataset/'
+pose_path = os.path.join(dataset_path, 'pose')
+non_pose_path = os.path.join(dataset_path, 'nonpose')
 
+# Path to pose folder
+pose_path = os.path.join(dataset_path, 'pose')
+# List all files in pose folder
+pose_files = os.listdir(pose_path)
 
-# Fungsi untuk menentukan label berdasarkan nama file
-def get_label(image_name, cutting_path, non_cutting_path):
-    if image_name in os.listdir(cutting_path):
-        return 'gun'
-    elif image_name in os.listdir(non_cutting_path):
-        return 'nongun'
+# Path to non-pose folder
+non_pose_path = os.path.join(dataset_path, 'nonpose')
+# List all files in non-pose folder
+non_pose_files = os.listdir(non_pose_path)
+
+# Display all file names in pose folder
+print("Files in pose folder:", pose_files)
+
+# Display all file names in non-pose folder
+print("Files in non_pose folder:", non_pose_files)
+
+# function to determine label based on name file
+def get_label(image_name, pose_path, non_pose_path):
+    if image_name in os.listdir(pose_path):
+        return 'pose'
+    elif image_name in os.listdir(non_pose_path):
+        return 'nonpose'
     else:
-        return None  # Tidak dapat menemukan file di kedua folder
+        return None  # Not found in both folders
 
-# Menambahkan kolom label berdasarkan nama folder
-df['label'] = df['image_name'].apply(lambda x: get_label(x, cutting_path, non_cutting_path))
+# Adds label column based on the folder name (pose or nonpose)
+df['label'] = df['image_name'].apply(lambda x: get_label(x, pose_path, non_pose_path))
 df.to_csv(f'{dataset_path}dataset.csv', index=False)
-df
